1e4466d565db9b7846e1bc626da75138ff2ff510
Emil Sahlén <emil.sahlen92@gmail.com>
2026-07-04 18:33:55 +0200

    add bundle splitting

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,27 @@ import babel from "@rolldown/plugin-babel";
 // https://vite.dev/config/
 export default defineConfig({
   publicDir: "../public",
-  base: "/",
+  base: "/Styrka/",
   assetsInclude: ["**/*.png", "**/*.jpg", "**/*.jpeg", "**/*.svg"],
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
   resolve: {
     alias: {
       "@assets": "/assets/",
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
     },
   },
 });
